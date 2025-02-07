707373a63fc0bc7a8389344150995791daf90f12
Benjamin Schwendner <schwendner@axinom.de>
2025-02-07 14:25:49 +0100

    feat: global rate limiter

--- services/payment-connector/service/src/stripe-domain/customer-overview-route.ts
+++ services/payment-connector/service/src/stripe-domain/customer-overview-route.ts
@@ -2,7 +2,6 @@ import { assertError, Logger } from '@axinom/mosaic-service-common';
 import cors from 'cors';
 import { Application, Request, Response } from 'express';
 import { Config, parseAuthenticationTokenValue } from '../common';
-import { limiter } from './rate-limit';
 import { getStripe } from './stripe-init';
 
 /**
@@ -53,6 +52,6 @@ export const customerOverviewRoute = (
       res.status(400).send([]);
     }
   };
-  app.get('/customer-overview', [cors(), limiter, customerOverviewMiddleware]);
+  app.get('/customer-overview', [cors(), customerOverviewMiddleware]);
   app.options('/customer-overview', [cors()]);
 };
